29cbdc2d4c29bba8428ac96d06c8567431b5dda8
Ezekiel Elin <ezekielelin@me.com>
2015-11-09 20:59:59 -0500

    Added templates for remaining files

---
+++ LAB5/src/MyAVLMap.java
@@ -0,0 +1,28 @@
+
+public class MyAVLMap implements MyMapADT {
+
+	@Override
+	public boolean insert(int k, int v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public RetVal find(int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RetVal delete(int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
